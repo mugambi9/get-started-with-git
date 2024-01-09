ab6c2b72d286246bcdd0363fcdc18f9dfa371af0
mugambi484 <123549183+mugambi484@users.noreply.github.com>
2024-01-09 12:05:43 +0300

    in

---
+++ development/java script/app.js
@@ -0,0 +1,71 @@
+//let condition = "rainy"
+//if(condition=="rainy"){
+  //  console.log("carry an umbrella")
+    //}else if (condition=="sunny"){
+      //      console.log("carry your sunglasses")
+
+        //}else{
+          //  console.log("its a chilled day")
+        //}
+    
+// function weatherforecast(condition){
+    
+// if(condition=="rainy"){
+//    console.log("carry an umbrella")
+//     }else if (condition=="sunny"){
+//            console.log("carry your sunglasses")
+
+//         }else{
+//            console.log("its a chilled day")
+//         }
+    
+// }
+// weatherforecastc
+
+// let calculator=function(num1,num2,operator){
+//     // add
+  
+//     if (operator=="+") {
+//         sum=num1+num2
+//         return sum 
+//     }
+// }
+// let results =calculator(45 , 6 ,"+")
+// console.log(results)
+
+function CanBeAllowEdentry( hasID , Appearance,Nationality ,Swahili){
+    if (hasID ==true&&Nationality=="kenyan")
+    console.log("Allow Entry")
+    else if ((Swahili==true || Nationality=="kenyan")&& Appearance=="old")
+    console.log("Allow Entry")
+else (
+    console.log("Deny entry")
+)
+}
+CanBeAllowEdentry ("Kenyan" ,false, false,"young")
+// function funcname(parameter){
+
+
+//  let myfunc =funcname(parameter){
+
+//  }
+//  let myArrowfunc=(par1,par2)=>{
+
+//  }
+// let addTwonumbers =(num1,num2)=>{
+//     let sum =num1+num2
+//     return sum
+// }
+
+
+// let addTwonumbers =(num1,num2)=>num1+num2
+// let result=addTwonumbers(22,7)
+// console.log(result)
+// for (let num =1;num<=10; num++){
+//     console.log(num)
+// }
+let num=1
+while(num <=100 ){
+    console.log(num)
+    num++
+}
---
+++ development/second-web/app.js
@@ -0,0 +1,13 @@
+console.log("hello")
+let age
+age=93
+let name= `your age is ${age}`
+/*tttt                          */
+console.log(name)
+let weight=39.5
+let signup=false
+age =34
+console.log(age)
+
+const pie =3.14
+console.log(pie)
